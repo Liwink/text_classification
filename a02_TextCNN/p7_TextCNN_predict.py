3ef91d5a2389c6ecf82eeaaaf1de4a326c57b9de
Liwink <lyh.next@gmail.com>
2017-07-19 13:27:45 +0800

    reformat p7_TextCNN_predict.py

--- a02_TextCNN/p7_TextCNN_predict.py
+++ a02_TextCNN/p7_TextCNN_predict.py
@@ -3,8 +3,8 @@
 # process--->1.load data(X:list of lint,y:int). 2.create session. 3.feed data. 4.predict
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 import tensorflow as tf
 import numpy as np
 # from p5_fastTextB_model import fastTextB as fastText
@@ -41,53 +41,59 @@ tf.app.flags.DEFINE_string("ckpt_dir2", "text_cnn_title_desc_checkpoint_exp/", "
 
 ##############################################################################################################################################
 filter_sizes = [1, 2, 3, 4, 5, 6, 7]  # [1,2,3,4,5,6,7]
+
+
 # 1.load data(X:list of lint,y:int). 2.create session. 3.feed data. 4.training (5.validation) ,(6.prediction)
 # 1.load data with vocabulary of words and labels
-vocabulary_word2index, vocabulary_index2word = create_voabulary(simple='simple',
-                                                                word2vec_model_path=FLAGS.word2vec_model_path,
-                                                                name_scope="cnn2")
-vocab_size = len(vocabulary_word2index)
-vocabulary_word2index_label, vocabulary_index2word_label = create_voabulary_label(name_scope="cnn2")
-questionid_question_lists = load_final_test_data(FLAGS.predict_source_file)
-test = load_data_predict(vocabulary_word2index, vocabulary_word2index_label, questionid_question_lists)
-testX = []
-question_id_list = []
-for tuple in test:
-    question_id, question_string_list = tuple
-    question_id_list.append(question_id)
-    testX.append(question_string_list)
-# 2.Data preprocessing: Sequence padding
-print("start padding....")
-testX2 = pad_sequences(testX, maxlen=FLAGS.sentence_len, value=0.)  # padding to max length
-print("end padding...")
-# 3.create session.
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-graph = tf.Graph().as_default()
-global sess
-global textCNN
-with graph:
-    sess = tf.Session(config=config)
-    # 4.Instantiate Model
-    textCNN = TextCNN(filter_sizes, FLAGS.num_filters, FLAGS.num_classes, FLAGS.learning_rate, FLAGS.batch_size,
-                      FLAGS.decay_steps, FLAGS.decay_rate,
-                      FLAGS.sentence_len, vocab_size, FLAGS.embed_size, FLAGS.is_training)
-    saver = tf.train.Saver()
-    if os.path.exists(FLAGS.ckpt_dir + "checkpoint"):
-        print("Restoring Variables from Checkpoint")
-        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.ckpt_dir))
-    else:
-        print("Can't find the checkpoint.going to stop")
-        # return
-# 5.feed data, to get logits
-number_of_training_data = len(testX2);
-print("number_of_training_data:", number_of_training_data)
+
+def load_data():
+    vocabulary_word2index, vocabulary_index2word = create_voabulary(simple='simple',
+                                                                    word2vec_model_path=FLAGS.word2vec_model_path,
+                                                                    name_scope="cnn2")
+    vocab_size = len(vocabulary_word2index)
+    vocabulary_word2index_label, vocabulary_index2word_label = create_voabulary_label(name_scope="cnn2")
+    questionid_question_lists = load_final_test_data(FLAGS.predict_source_file)
+    test = load_data_predict(vocabulary_word2index, vocabulary_word2index_label, questionid_question_lists)
+    testX = []
+    question_id_list = []
+    for tuple in test:
+        question_id, question_string_list = tuple
+        question_id_list.append(question_id)
+        testX.append(question_string_list)
+    # 2.Data preprocessing: Sequence padding
+    print("start padding....")
+    testX2 = pad_sequences(testX, maxlen=FLAGS.sentence_len, value=0.)  # padding to max length
+    print("end padding...")
+    # 3.create session.
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    graph = tf.Graph().as_default()
+    global sess
+    global textCNN
+    with graph:
+        sess = tf.Session(config=config)
+        # 4.Instantiate Model
+        textCNN = TextCNN(filter_sizes, FLAGS.num_filters, FLAGS.num_classes, FLAGS.learning_rate, FLAGS.batch_size,
+                          FLAGS.decay_steps, FLAGS.decay_rate,
+                          FLAGS.sentence_len, vocab_size, FLAGS.embed_size, FLAGS.is_training)
+        saver = tf.train.Saver()
+        if os.path.exists(FLAGS.ckpt_dir + "checkpoint"):
+            print("Restoring Variables from Checkpoint")
+            saver.restore(sess, tf.train.latest_checkpoint(FLAGS.ckpt_dir))
+        else:
+            print("Can't find the checkpoint.going to stop")
+            # return
+    # 5.feed data, to get logits
+    number_of_training_data = len(testX2)
+    print("number_of_training_data:", number_of_training_data)
+    return vocabulary_index2word_label, testX2
 
 
 # index = 0
 # predict_target_file_f = codecs.open(FLAGS.predict_target_file, 'a', 'utf8')
 #############################################################################################################################################
 def get_logits_with_value_by_input(start, end):
+    vocabulary_index2word_label, testX2 = load_data()
     x = testX2[start:end]
     global sess
     global textCNN
@@ -156,8 +162,8 @@ def get_label_using_logits(logits, vocabulary_index2word_label, top_number=5):
     label_list = []
     for index in index_list:
         label = vocabulary_index2word_label[index]
-        label_list.append(
-            label)  # ('get_label_using_logits.label_list:', [u'-3423450385060590478', u'2838091149470021485', u'-3174907002942471215', u'-1812694399780494968', u'6815248286057533876'])
+        # ('get_label_using_logits.label_list:', [u'-3423450385060590478', u'2838091149470021485', u'-3174907002942471215', u'-1812694399780494968', u'6815248286057533876'])
+        label_list.append(label)
     return label_list
 
 
@@ -169,8 +175,8 @@ def get_label_using_logits_with_value(logits, vocabulary_index2word_label, top_n
     label_list = []
     for index in index_list:
         label = vocabulary_index2word_label[index]
-        label_list.append(
-            label)  # ('get_label_using_logits.label_list:', [u'-3423450385060590478', u'2838091149470021485', u'-3174907002942471215', u'-1812694399780494968', u'6815248286057533876'])
+        # ('get_label_using_logits.label_list:', [u'-3423450385060590478', u'2838091149470021485', u'-3174907002942471215', u'-1812694399780494968', u'6815248286057533876'])
+        label_list.append(label)
         value_list.append(logits[index])
     return label_list, value_list
 
