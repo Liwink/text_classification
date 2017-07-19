d5c9dbd63ddac6aa09fe6d32b689c118bd6ee4a1
Liwink <lyh.next@gmail.com>
2017-07-19 18:01:44 +0800

    add evaluation

---
+++ a02_TextCNN/evaluate.py
@@ -0,0 +1,30 @@
+import math
+
+def evaluate(predict_label_and_marked_label_list):
+    """
+    :param predict_label_and_marked_label_list: 一个元组列表。例如
+    [ ([1, 2, 3, 4, 5], [4, 5, 6, 7]),
+      ([3, 2, 1, 4, 7], [5, 7, 3])
+     ]
+    需要注意这里 predict_label 是去重复的，例如 [1,2,3,2,4,1,6]，去重后变成[1,2,3,4,6]
+    :return:
+    """
+    right_label_num = 0
+    right_label_at_pos_num = [0, 0, 0, 0, 0]
+    sample_num = 0
+    all_marked_label_num = 0
+    for predict_labels, marked_labels in predict_label_and_marked_label_list:
+        sample_num += 1
+        marked_label_set = set(marked_labels)
+        all_marked_label_num += len(marked_label_set)
+        for pos, label in zip(range(0, min(len(predict_labels), 5)), predict_labels):
+            if label in marked_label_set:
+                right_label_num += 1
+                right_label_at_pos_num[pos] = right_label_at_pos_num[pos] + 1
+
+    precision = 0.0
+    for pos, right_num in zip(range(0, 5), right_label_at_pos_num):
+        precision += ((right_num / float(sample_num))) / math.log(2.0 + pos)
+    recall = float(right_label_num) / all_marked_label_num
+
+    return (precision * recall) / (precision + recall), precision, recall, right_label_num, all_marked_label_num
